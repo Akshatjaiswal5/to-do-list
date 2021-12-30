46d3778624cc3eaaaa1058d81a7ca8c7270b4458
DESKTOP-T526RFM\lenovo <akshatjaiswal521@gmail.com>
2021-12-30 13:46:43 +0530

    initial files

---
+++ main.js
@@ -0,0 +1,34 @@
+let list=[];
+const data=document.querySelector('.data');
+
+const remove= function(i){
+ list.splice(i,1);
+ window.localStorage.setItem('savedList',JSON.stringify(list));
+ refresh();
+}
+const refresh= function(){
+ data.innerHTML=``;
+ list.forEach((string,i)=>{
+  data.innerHTML=data.innerHTML+`<li>${string}</li> <button onclick='remove(${i})' class="delbutton">X</button>`;
+ })
+}
+const addItem=function(){
+ let string=document.querySelector('.addbox').value;
+ string=string.trim();
+ if(string==='')
+  return;
+ list.push(string);
+ window.localStorage.setItem('savedList',JSON.stringify(list));
+ document.querySelector('.addbox').value='';
+ refresh();
+}
+
+const initialize=function(){
+ s=window.localStorage.getItem('savedList');
+ list=JSON.parse(s);
+ refresh();
+}
+
+window.onload=initialize();
+
+
